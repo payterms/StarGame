333c9cfb0e257e4ac357a381b1fc3d90e32ac19d
Michael V. Gritsenko <payterms@mail.ru>
2019-04-06 17:34:16 +0300

    Initial commit

---
+++ core/src/ru/payts/StarGame.java
@@ -0,0 +1,40 @@
+package ru.payts;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		region = new TextureRegion(img, 100, 100, 150, 150);
+		region.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.setColor(0.1f, 0.1f, 0.1f, 1f);
+		batch.draw(img, 100, 100);
+		batch.setColor(0.666f, 0.777f, 0.1313f, 1f);
+		batch.draw(region, 0, 0);
+		batch.end();
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/ru/payts/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.payts.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.payts.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
