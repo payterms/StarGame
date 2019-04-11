5792b44c4db75df90455b9a45de983d855801f9a
Michael V. Gritsenko <payterms@mail.ru>
2019-04-11 19:47:06 +0300

    Lesson 2 HW

--- core/src/ru/payts/Screen/MenuScreen.java
+++ core/src/ru/payts/Screen/MenuScreen.java
@@ -11,7 +11,10 @@ public class MenuScreen extends BaseScreen {
     private Vector2 touch;
     private Vector2 pos;
     private Vector2 v;
+    private Vector2 buf;
     private Texture img;
+    private float screenX = 0;
+    private float screenY = 0;
 
     @Override
     public void show() {
@@ -19,19 +22,23 @@ public class MenuScreen extends BaseScreen {
         touch = new Vector2();
         pos = new Vector2();
         v = new Vector2(0.5f, 0.5f);
+        buf = new Vector2();
         img = new Texture("badlogic.jpg");
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        pos.add(v);
+        buf.set(touch);
+        if (buf.sub(pos).len() > 0.5f) {
+            pos.add(v);
+        } else {
+            pos.set(touch);
+        }
         batch.begin();
         batch.draw(img, pos.x, pos.y);
         batch.end();
-        if (pos.y > Gdx.graphics.getHeight() - 256) {
-            v.setZero();
-        }
+        
     }
 
     @Override
@@ -43,6 +50,7 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos).setLength(0.5f));
         System.out.println("touchDown touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
